25ddce8e8fd0c225644d6ca523d3db8a5040797c
Paulo Matos <paulojomatos@gmail.com>
2023-08-22 22:17:14 -0300

    Create best_time_to_buy_stock.py

---
+++ python/leetcode/best_time_to_buy_stock.py
@@ -0,0 +1,19 @@
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock
+
+# O(1)
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        best_day_to_buy = 0
+        best_day_to_sell = 0
+        max_profit = 0
+
+        for i in range(len(prices)):
+            current_profit = prices[i] - prices[best_day_to_buy]
+            if current_profit > max_profit:
+                max_profit = current_profit
+                best_day_to_sell = i
+            
+            if prices[i] < prices[best_day_to_buy]:
+                best_day_to_buy = i
+
+        return max_profit
