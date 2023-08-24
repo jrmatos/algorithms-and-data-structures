ba33dcc4b418c2f6721b9be8f2edcac78cf6774a
Paulo Matos <paulojomatos@gmail.com>
2023-08-24 10:34:42 -0300

    Update bubble_sort.py

--- python/bubble_sort.py
+++ python/bubble_sort.py
@@ -1,9 +1,8 @@
 from data import average, best, worst
 
-# TODO: make it a real bubblesort
 def bubble_sort(arr):
     for i in range(len(arr)):
-        for j in range(len(arr)):
+        for j in range(i+1, len(arr)):
             if arr[j] > arr[i]:
                 arr[i], arr[j] = arr[j], arr[i]
 
