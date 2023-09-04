eea6282e7d6889c7b9867bb055d6526fbd663c77
Paulo J. Matos <paulojomatos@gmail.com>
2023-09-04 13:03:10 -0300

    better solution for jump game

--- python/leetcode/jump_game.py
+++ python/leetcode/jump_game.py
@@ -25,3 +25,26 @@ class Solution:
             
             if curr > extra_steps:
                 extra_steps = curr
+
+"""
+Better solution going backwards.
+
+Runtime
+Details
+389ms
+Beats 94.37%of users with Python3
+Memory
+Details
+17.42MB
+Beats 90.03%of users with Python3
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        last_good_index = len(nums) - 1
+
+        for i in range(len(nums) - 1, -1, -1):
+            if i + nums[i] >= last_good_index:
+                last_good_index = i
+
+        return last_good_index == 0
+"""
+
